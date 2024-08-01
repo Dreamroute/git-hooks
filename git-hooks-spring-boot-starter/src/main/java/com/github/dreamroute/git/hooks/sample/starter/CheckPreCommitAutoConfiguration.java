6ec8e3dc3f38804f63259da9a98e0859bfeb3e1a
wangdehai <wangdehai@yzw.cn>
2024-08-01 10:39:30 +0800

    m

--- git-hooks-spring-boot-starter/src/main/java/com/github/dreamroute/git/hooks/sample/starter/CheckPreCommitAutoConfiguration.java
+++ git-hooks-spring-boot-starter/src/main/java/com/github/dreamroute/git/hooks/sample/starter/CheckPreCommitAutoConfiguration.java
@@ -1,23 +1,36 @@
 package com.github.dreamroute.git.hooks.sample.starter;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.ArrayUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
 
 @Slf4j
 @Component
-@Profile("local")
 public class CheckPreCommitAutoConfiguration implements CommandLineRunner {
+
+    @Resource
+    private Environment environment;
+
     @Override
     public void run(String... args) {
+        String[] activeProfiles = environment.getActiveProfiles();
+        if (ArrayUtil.isEmpty(activeProfiles)) {
+            throw new IllegalArgumentException("请指定启动环境信息active profiles");
+        }
+        boolean needCheck = Arrays.stream(activeProfiles)
+            .anyMatch(e -> e.contains("local") || e.contains("qa") || e.contains("dev"));
         String baseDir = System.getProperty("user.dir");
         baseDir += "/.git/hooks/pre-commit";
         boolean exist = FileUtil.exist(baseDir);
-        if (!exist) {
-            String h = StrUtil.repeat('-', 50);
+        if (needCheck && !exist) {
+            String h = CharSequenceUtil.repeat('-', 50);
             log.error("\r\n" + h + "\r\n请先执行[mvn clean] 或 [mvn install]安装pre-commit\r\n" + h);
             System.exit(0);
         }
